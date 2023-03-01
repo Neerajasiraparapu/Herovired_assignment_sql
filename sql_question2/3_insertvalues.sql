1c7857b1cdbc280bbc3e83b0152c9341996d46f5
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2023-03-01 22:48:32 +0530

    sql_question2

---
+++ sql_question2/3_insertvalues.sql
@@ -0,0 +1,10 @@
+insert into FootballVenue values ( 20001,'France',10003,42115);
+insert into FootballVenue values ( 20002,'Germany',10007,42124);
+insert into FootballVenue values ( 20003,'India',10011,52623);
+insert into FootballVenue values ( 20004,'Austrila',10015,76383);
+insert into FootballVenue values ( 20005,'England',10026,45726);
+insert into FootballVenue values ( 20006,'Europe',10038,67373);
+insert into FootballVenue values ( 20007,'New York',10054,92728);
+insert into FootballVenue values ( 20008,'USA',10059,64732);
+insert into FootballVenue values ( 20009,'Dubai',10067,73873);
+insert into FootballVenue values ( 20010,'England',10078,56873);
