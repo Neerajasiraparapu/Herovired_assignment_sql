40c047a6aeb9e9227d8d3394e5d2c62d896ade8d
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2023-03-01 23:08:13 +0530

    sql_question1

--- sql_question1/2_createtable.sql
+++ sql_question1/2_createtable.sql
@@ -1,1 +1,1 @@
-create table Employee_Details(emp_id number(5),emp_name varchar2(10),job_name varchar2(20),manager_id number(5),hire_date varchar2(10),salary number(8),dep_id number(5));
+create table EmployeeDetails(emp_id number(5),emp_name varchar2(10),job_name varchar2(20),manager_id number(5),hire_date varchar2(10),salary number(8),dep_id number(5));
