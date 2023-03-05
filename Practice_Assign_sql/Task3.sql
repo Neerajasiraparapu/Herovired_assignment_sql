6026133fc8f648bc6ff25979929f87214d7ec636
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2023-03-05 16:40:12 +0530

    prac_ass2_sql

--- Practice_Assign_sql/Task3.sql
+++ Practice_Assign_sql/Task3.sql
@@ -17,5 +17,5 @@ from customer c INNER JOIN salesman s on c.salesman_id=s.salesman_id;
 /*15)Write a SQL query to find salespeople who received commissions of more than 12 percent from the company. Return Customer Name, 
 customer city, Salesman, and commission.*/
 
-query:- select c.cust_name as "customer name",c.city,s.name as "salesman",s.city,s.commission 
-from customer c INNER JOIN salesman s on c.salesman_id=s.salesman_id where s.commission>0.12 and c.city<>s.city;
+query:- select c.cust_name as "customer name",c.city,s.name as "salesman",s.commission 
+from customer c INNER JOIN salesman s on c.salesman_id=s.salesman_id where s.commission>0.12;
